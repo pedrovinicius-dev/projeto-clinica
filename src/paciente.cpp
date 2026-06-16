c9ed04b4e157bc3916fb1362df8a239c0dfb3fdd
Anderson Moreno <andermrn2004@gmail.com>
2026-06-16 15:27:09 -0300

    Deixando o codigo formatado

--- src/paciente.cpp
+++ src/paciente.cpp
@@ -4,47 +4,61 @@
 #include <locale.h>
 #include "paciente.h"
 
-
 using namespace std;
 
-     struct paciente{
-        string nome;
-        int idade;
-        bool prioridade;
-    };
-  
-void principal() {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+struct paciente
+{
+  string nome;
+  int idade;
+  bool prioridade;
+};
+
+void principal()
+{
+  setlocale(LC_ALL, "pt_BR.UTF-8");
   string name;
-  int age,N, prioridade_inteira;
+  int age, N, prioridade_inteira; // N = numeros de paciente
   bool priority;
-  cout<<"digite o número de pacientes:\n";
-  cin>>N;
-  vector<paciente> lista;
-  for(int i =0;i<N;i++){
-    cout<<"nome:";
-    cin>>name;
-    cout<<"idade:";
-    cin>>age;
-    cout<<"possui prioridade?\n1-sim\n2-não\n";
-    cin>>prioridade_inteira;
-    if(prioridade_inteira ==1){
-        priority=true;
-    }else{priority=false;}
+  cout << "\n===========================================\n\tCADASTRO DE PACIENTE\n===========================================\n\nDigite a quantidade de pacientes: > ";
+  cin >> N; // Ler numeros de paciente
+
+  vector<paciente> lista; // Guarda as informaçoes dos pacientes
+  for (int i = 0; i < N; i++)
+  {
+    cout << "Nome:";
+    cin >> name;
+    cout << "Idade:";
+    cin >> age;
+    cout << "Possui prioridade?\n1 - Sim\n2 - Não\n";
+    cin >> prioridade_inteira;
+
+    if (prioridade_inteira == 1)
+      priority = true;
+
+    else
+    {
+      priority = false;
+    }
     paciente N;
     N.prioridade = priority;
     N.nome = name;
     N.idade = age;
     lista.push_back(N);
   }
-  cout<<"===========================\nlista de pacientes\n";
-  for(int i =0;i<lista.size();i++){
-    cout<<"|paciente "<<i+1<<"|\n";
-    cout<<"nome: "<<lista[i].nome<<"\n";
-    cout<<"idade: "<<lista[i].idade<<"\n";
-    if(lista[i].prioridade==true){
-        cout<<"prioridade:sim\n";
-    }else{cout<<"prioridade:não\n";}
-  } 
 
+  cout << "===========================\nlista de pacientes\n";
+  for (int i = 0; i < lista.size(); i++) //Mostra 
+  {
+    cout << "| Paciente " << i + 1 << " |\n";
+    cout << "Nome: " << lista[i].nome << "\n";
+    cout << "Idade: " << lista[i].idade << "\n";
+    if (lista[i].prioridade == true)
+    {
+      cout << "Prioridade: Sim\n";
+    }
+    else
+    {
+      cout << "Prioridade: Não\n";
+    }
+  }
 }
