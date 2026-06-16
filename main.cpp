95996dfab11f1f6029c3b96abd296375339963c8
Anderson Moreno <andermrn2004@gmail.com>
2026-06-15 22:03:28 -0300

    teste de commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <locale.h>
+using namespace std;
 
-int main(){
-    setlocale(LC_ALL, "pt_BR.UTF-8");
-    std::string paciente;
-    std::cout<<"bem vindo a clinica do eaj\ndigite o nome do paciente";
-    std::cin>>paciente;
-
+int main()
+{
+    cout << "Teste";
 }
