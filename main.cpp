38dca6c1f3f61eb6723bedbefe742f399f829e00
pedrovinicius-dev <pedrovini423@gmail.com>
2026-06-15 23:02:39 -0300

    menu_de_escolha

--- main.cpp
+++ main.cpp
@@ -12,21 +12,23 @@ int main()
 void Iniciar_programa()
 {
     int opcao;
-    do
-    {
+
+    for(int i=0;i<2;i++){
         cout << "\n===========================================\n\tCLINICA EAJ\n===========================================\n\n1 - Cadastrar Paciente\n2 - Chamar Próximo paciente\n3 - Visualizar fila\n4 - Pesquisar paciente\n5 - Listar pacientes\n6 - Relátorio por médico\n7 - Salvar dados\n8 - Carregar dados\n0 - Sair\n\nEscolha uma opcao: ";
         cin >> opcao;
-    } while (opcao != 0);
-
-    switch (opcao)
+        switch (opcao)
     {
     case 1:
         //cadastro de paciente
         break;
-    
+    case 2:
+        //chamar proximo paciente
+        break;
     default:
         cout << "\nOpção invalida! Digite novamente...\n";
         break;
     }
+    i++;
+    }
+    }
     
-}
