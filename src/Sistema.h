aabfcb28b7545c0dc1f348448de72a7c55b47b7e
Anderson Moreno <andermrn2004@gmail.com>
2026-06-18 01:13:29 -0300

    Adicionando Sistema.h e Sistema.cpp

---
+++ src/Sistema.h
@@ -0,0 +1,14 @@
+#ifndef SISTEMA_H
+#define SISTEMA_H
+
+#include <iostream>
+#include <vector>
+#include <locale.h>
+#include "paciente.h"
+using namespace std;
+
+//Vector globais
+extern vector<Paciente> ListaGeral; //Vai guardar todos os pacientes do sistema
+extern vector<Paciente> FilaEspera; //Entra aqui quando o paciente e cadastrado, quando ele for chamado (op2), ele é removido da fila
+
+#endif
