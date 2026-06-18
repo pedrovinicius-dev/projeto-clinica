0c0fe7390bbd39a832ce8d738973497f7d325b79
andermrn <andermoreno2004@gmail.com>
2026-06-18 03:58:32 -0300

    Correção UTF-8

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8); //ambas as linhas sao para usar o UTF-8
     SetConsoleCP(CP_UTF8);
     
     setlocale(LC_ALL, "pt_BR.UTF-8");
